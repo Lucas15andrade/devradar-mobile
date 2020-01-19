64dbeceacd77305d09ebd2232d271c0f49fba205
lucas15andrade <lap.andrade15@gmail.com>
2020-01-18 22:13:44 -0300

    Versão final

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,6 +5,7 @@ import {requestPermissionsAsync, getCurrentPositionAsync} from 'expo-location';
 import {MaterialIcons} from '@expo/vector-icons';
 
 import api from '../services/api';
+import {connect, disconnect, subscribeToNewsDevs} from '../services/socket';
 
 function Main({navigation}){
 
@@ -36,6 +37,19 @@ function Main({navigation}){
         loadInitialPosition();
     }, []);
 
+    useEffect(() => {
+        subscribeToNewsDevs(dev => setDevs([...devs, dev]));
+    }, [devs]);
+
+    function setupWebSocket(){
+
+        disconnect();
+
+        const {latitude, longitude} = currentRegion;
+
+        connect(latitude, longitude, techs);
+    }
+
     async function loadDevs(){
         const {latitude, longitude} = currentRegion;
 
@@ -48,6 +62,7 @@ function Main({navigation}){
         });
 
         setDevs(response.data.devs);
+        setupWebSocket();
     }
 
     function handleRegionChanged(region){
